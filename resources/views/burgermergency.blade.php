8ae5add6c97024cacc51be4f181a29cef5d18109
Matt Stauffer <matt@tighten.co>
2016-07-17 08:30:59 -0400

    logo

--- resources/views/burgermergency.blade.php
+++ resources/views/burgermergency.blade.php
@@ -45,18 +45,33 @@
             .location-box input, .location-box button {
                 font-size: 38px;
                 margin-left: 1em;
+                padding: 0.25em;
+            }
+
+            .location-box button {
+                background: #eee;
+                border: 1px solid #999;
+                font-size: 24px;
+                margin-top: 1em;
+                padding: 0.5em;
+            }
+
+            .burger {
+                max-height: 40vh;
+                margin: 0 auto;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
+                <img src="burger.png" alt="Burger" class="burger"><br><br>
                 <div class="title">It's a burgermergency!</div>
                 @php
                     $first = current($results->businesses)
                 @endphp
                 <div class="direction">Go here now: <a href="{{ $first->url }}">{{ $first->name }}</a></div>
-                <div class="location-box"><form action="/search" method="POST">{{ csrf_field() }}<input type="text" name="location" value="{{ $search }}" autofocus><button>Update Location</button></form></div>
+                <div class="location-box"><form action="/search" method="POST">{{ csrf_field() }}<input type="text" name="location" value="{{ $search }}" autofocus><br><button>Update Location</button></form></div>
 
                 <br>
                 <iframe
@@ -66,6 +81,8 @@
                   src="https://www.google.com/maps/embed/v1/place?key={{ config('services.google.maps') }}&q=burgers
                   &center={{ $results->region->center->latitude }},{{ $results->region->center->longitude }}" allowfullscreen>
                 </iframe>
+
+                <div class="credits">Logo by stolkramaker for the Noun Project</div>
             </div>
         </div>
     </body>
